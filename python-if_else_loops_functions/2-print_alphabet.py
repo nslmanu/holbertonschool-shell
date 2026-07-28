dab7c182be12efdcee85869f9bc7c8f2fe220877
nslmanu <nslmanu@me.com>
2026-07-28 08:34:21 +0000

    first commit

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-fruits = ["a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-for x in fruits:
+alpha = ["a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for x in alpha:
   print(x, end="")
-
-
-
