8d345885b2b4626ca1802e904b90c18e81acbb31
nslmanu <nslmanu@me.com>
2026-07-28 08:56:28 +0000

    first commit

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for x in ["a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
-  print(x,end='')
-
+for x in range(97, 123):
+#for x in ['a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
+ print("{}".format(chr(x)),end="")
