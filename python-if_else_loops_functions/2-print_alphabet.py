05d388f78d0056f76a1619eba011f306b4e91a37
nslmanu <nslmanu@me.com>
2026-07-28 08:37:34 +0000

    first commit

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-alpha = ["a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-for x in alpha:
+for x in ["a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
   print(x, end="")
+
